2e55f06e52f1e44266c487dcf0c46ea013c61e93
madina1212 <halikovamadina656@gmail.com>
2022-11-21 12:03:12 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        Days days = Days.ЖЕКШЕМБИ;
+        switch (days){
+            case ДҮЙШӨМБҮ:
+                System.out.println("жава окуйм");
+                break;
+            case ШЕЙШЕМБИ:
+                System.out.println("ПРАКТИК");
+                break;
+            case ШАРШЕМБИ:
+                System.out.println("англис тили сабагын уйроном");
+                break;
+            case БЕЙШЕМБИ:
+                System.out.println("ПРАКТИК");
+                break;
+            case ЖУМА:
+                System.out.println("EVENT");
+                break;
+            case ИШЕМБИ:
+                System.out.println("дем алыш");
+                break;
+            case ЖЕКШЕМБИ:
+                System.out.println("дем алыш");
+                break;
+        }
+    }
+}
